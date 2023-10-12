5723cf1d7ff9d0407046d78ae5e372c5b975933a
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2023-10-12 23:04:57 +0200

    Add Sentry error monitoring

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import { ApolloServer } from "apollo-server";
+import * as Sentry from "@sentry/node";
+import { ProfilingIntegration } from "@sentry/profiling-node";
 import { typeDefs } from "./schema";
 import { resolvers } from "./resolvers";
 import { contextFactory } from "./context";
+
+Sentry.init({
+  dsn: "https://5afc1975164a2c5f1f04878cf842a565@o4506037007810560.ingest.sentry.io/4506039360815104",
+  integrations: [new ProfilingIntegration()],
+  // Performance Monitoring
+  tracesSampleRate: 0.5, // Capture 100% of the transactions, reduce in production!
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 0.5, // Capture 100% of the transactions, reduce in production!
+});
 
 const port = process.env.PORT || 3001;
 
